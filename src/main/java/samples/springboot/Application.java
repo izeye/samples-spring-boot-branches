e5e0be3955ebd5474058fecf4f195bbd6f71a7ee
izeye <izeye@naver.com>
2015-01-31 12:51:11 +0900

    Add a Spring Boot sample doing nothing.

---
+++ src/main/java/samples/springboot/Application.java
@@ -0,0 +1,16 @@
+package samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by izeye on 15. 1. 31..
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
