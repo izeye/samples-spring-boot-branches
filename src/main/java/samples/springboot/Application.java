c7bd52ccc52e0517e91c31da72515ca54954b6ca
izeye <izeye@naver.com>
2015-02-05 17:34:52 +0900

    Add a Spring Boot Admin sample having many problems.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -1,12 +1,16 @@
 package samples.springboot;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by izeye on 15. 1. 31..
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
