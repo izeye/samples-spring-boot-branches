d7dc06c726256db9d19136beff2624af3e56f6f3
izeye <izeye@naver.com>
2015-02-08 17:18:13 +0900

    Add a Spring Boot Thymeleaf sample with legacy HTML5 mode.

---
+++ src/main/java/samples/springboot/HomeController.java
@@ -0,0 +1,19 @@
+package samples.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by izeye on 15. 2. 8..
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+
+}
