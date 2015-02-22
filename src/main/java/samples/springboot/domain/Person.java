940999b4584bb3cc077187b744565df889dbe7e9
izeye <izeye@naver.com>
2015-02-22 15:56:07 +0900

    Apply Lombok.

--- src/main/java/samples/springboot/domain/Person.java
+++ src/main/java/samples/springboot/domain/Person.java
@@ -1,58 +1,16 @@
 package samples.springboot.domain;
 
+import lombok.Data;
+
 /**
  * Created by izeye on 15. 2. 22..
  */
+@Data
 public class Person {
 
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private int age;
-
-	public Person() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "Person{" +
-				"id=" + id +
-				", firstName='" + firstName + '\'' +
-				", lastName='" + lastName + '\'' +
-				", age=" + age +
-				'}';
-	}
 
 }
