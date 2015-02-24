18e402f806d92c00a9c88aaba99ecc3fb3dee9fd
izeye <izeye@naver.com>
2015-02-24 23:20:54 +0900

    Use the Spring Boot's auto-configured JdbcTemplate.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -2,21 +2,12 @@ package samples.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 /**
  * Created by izeye on 15. 1. 31..
  */
 @SpringBootApplication
 public class Application {
-
-	@Bean
-	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
