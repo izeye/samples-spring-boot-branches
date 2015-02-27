988e99f21914167c6e2ca998c4964ad4e8caae0f
izeye <izeye@naver.com>
2015-02-27 14:41:32 +0900

    Apply Spring Data REST.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -1,8 +1,11 @@
 package samples.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by izeye on 15. 1. 31..
@@ -10,16 +13,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	private CustomerRepository repository;
+
+	@PostConstruct
+	protected void init() {
+		repository.save(new Customer("Jack", "Bauer"));
+		repository.save(new Customer("Chloe", "O'Brian"));
+		repository.save(new Customer("Kim", "Bauer"));
+		repository.save(new Customer("David", "Palmer"));
+		repository.save(new Customer("Michelle", "Dessler"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
 		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
-
-		try {
-			Thread.sleep(Integer.MAX_VALUE);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
--- src/main/java/samples/springboot/CustomerRepository.java
+++ src/main/java/samples/springboot/CustomerRepository.java
@@ -1,13 +1,13 @@
 package samples.springboot;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 /**
  * Created by izeye on 15. 2. 27..
  */
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	List<Customer> findByLastName(String lastName);
 
