fc91043526c140a5282ba3f15a4be2a20bfec1fc
izeye <izeye@naver.com>
2015-02-27 16:37:33 +0900

    Change to expose IDs for `Customer`s.

---
+++ src/main/java/samples/springboot/SpringDataRestConfig.java
@@ -0,0 +1,18 @@
+package samples.springboot;
+
+import org.springframework.boot.autoconfigure.data.rest.SpringBootRepositoryRestMvcConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+/**
+ * Created by izeye on 15. 2. 27..
+ */
+@Configuration
+public class SpringDataRestConfig extends SpringBootRepositoryRestMvcConfiguration {
+
+	@Override
+	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Customer.class);
+	}
+
+}
