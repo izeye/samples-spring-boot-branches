f06bea071f42db6f19d1d944cf76406ebe25c298
izeye <izeye@naver.com>
2015-03-02 00:42:28 +0900

    Apply integration test for testing Coveralls.

--- src/test/java/samples/springboot/ApplicationTests.java
+++ src/test/java/samples/springboot/ApplicationTests.java
@@ -1,10 +1,15 @@
 package samples.springboot;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by izeye on 15. 3. 1..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
 
 	@Test
