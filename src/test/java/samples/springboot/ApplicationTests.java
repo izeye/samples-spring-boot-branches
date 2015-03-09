c352402ca419fa2e880ef49b4f7850d2891cf9a2
izeye <izeye@naver.com>
2015-03-10 01:56:49 +0900

    Add a DataSource test.

---
+++ src/test/java/samples/springboot/ApplicationTests.java
@@ -0,0 +1,34 @@
+package samples.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+/**
+ * Created by izeye on 15. 3. 10..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void test() {
+		System.out.println(dataSource);
+
+		assertThat(
+				dataSource,
+				is(instanceOf(org.apache.tomcat.jdbc.pool.DataSource.class)));
+	}
+
+}
