03ba89866fd3a45ed058bd7816b5cb8458c3fa6f
izeye <izeye@naver.com>
2015-03-11 07:40:55 +0900

    Upgrade to Spring Boot 1.2.2.RELEASE.

---
+++ src/test/java/samples/springboot/HomeControllerTests.java
@@ -0,0 +1,34 @@
+package samples.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by izeye on 15. 3. 11..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest(randomPort = true)
+public class HomeControllerTests {
+
+	@Value("${local.server.port}")
+	int port;
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void test() {
+		String response = restTemplate.getForObject(
+				"http://localhost:{port}/", String.class, port);
+		assertThat(response, is("Hello World!"));
+	}
+
+}
