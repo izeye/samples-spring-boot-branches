c886ada797f08ea04c3139456c7e9fda626c863d
izeye <izeye@naver.com>
2015-03-12 10:54:29 +0900

    Add a RestTemplate test.

--- src/test/java/samples/springboot/RestTemplateTests.java
+++ src/test/java/samples/springboot/RestTemplateTests.java
@@ -6,12 +6,20 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.fail;
 
 /**
  * Created by izeye on 15. 3. 12..
  */
 public class RestTemplateTests {
+
+	private static final long DEFAULT_CONNECTION_TIMEOUT = 1000;
+	private static final long DEFAULT_READ_TIMEOUT = 1000;
 
 	RestTemplate restTemplate;
 
@@ -20,8 +28,8 @@ public class RestTemplateTests {
 		restTemplate = new RestTemplate();
 		SimpleClientHttpRequestFactory requestFactory
 				= (SimpleClientHttpRequestFactory) restTemplate.getRequestFactory();
-		requestFactory.setConnectTimeout(1000);
-		requestFactory.setReadTimeout(1000);
+		requestFactory.setConnectTimeout((int) DEFAULT_CONNECTION_TIMEOUT);
+		requestFactory.setReadTimeout((int) DEFAULT_READ_TIMEOUT);
 	}
 
 	@Test
@@ -34,6 +42,10 @@ public class RestTemplateTests {
 		}
 		long elapsedTime = System.currentTimeMillis() - startTime;
 		System.out.println(elapsedTime);
+		assertThat(
+				elapsedTime,
+				is(both(greaterThan(DEFAULT_CONNECTION_TIMEOUT))
+						.and(lessThan(DEFAULT_CONNECTION_TIMEOUT + 1000))));
 	}
 
 }
