e7b3cc2411cd63998cd16b43730a4b0f0be2ec0c
izeye <izeye@naver.com>
2015-03-21 19:07:44 +0900

    Add an `@EnableAsync` test.

--- src/test/java/samples/springboot/ApplicationTests.java
+++ src/test/java/samples/springboot/ApplicationTests.java
@@ -2,8 +2,19 @@ package samples.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
+import org.springframework.scheduling.annotation.ProxyAsyncConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNull.nullValue;
 
 /**
  * Created by izeye on 15. 3. 1..
@@ -12,8 +23,33 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	ProxyAsyncConfiguration proxyAsyncConfiguration;
+
+	@Autowired
+	AsyncAnnotationBeanPostProcessor asyncAnnotationBeanPostProcessor;
+
 	@Test
 	public void test() {
+		for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(beanDefinitionName);
+			Object bean = applicationContext.getBean(beanDefinitionName);
+			System.out.println(bean.getClass());
+		}
+	}
+
+	@Test
+	public void testAsync() {
+		Object executor = ReflectionTestUtils.getField(proxyAsyncConfiguration, "executor");
+		assertThat(executor, is(nullValue()));
+
+		Object advisor = ReflectionTestUtils.getField(asyncAnnotationBeanPostProcessor, "advisor");
+		Object advice = ReflectionTestUtils.getField(advisor, "advice");
+		Object defaultExecutor = ReflectionTestUtils.getField(advice, "defaultExecutor");
+		assertThat(defaultExecutor, is(instanceOf(SimpleAsyncTaskExecutor.class)));
 	}
 
 }
