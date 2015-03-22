5fcc262da8516cf441494b08d15d4e2288647c68
izeye <izeye@naver.com>
2015-03-22 12:55:57 +0900

    Fix bug.

--- src/main/java/samples/springboot/config/AsyncConfig.java
+++ src/main/java/samples/springboot/config/AsyncConfig.java
@@ -1,5 +1,7 @@
 package samples.springboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,9 +14,17 @@ import java.util.concurrent.Executor;
 @Configuration
 public class AsyncConfig extends AsyncConfigurerSupport {
 
+	@Bean
+	public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+		return new ThreadPoolTaskExecutor();
+	}
+
+	@Autowired
+	private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
 	@Override
 	public Executor getAsyncExecutor() {
-		return new ThreadPoolTaskExecutor();
+		return threadPoolTaskExecutor;
 	}
 
 }
