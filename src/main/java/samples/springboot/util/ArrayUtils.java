437183ded2ab49ee71a76a11650c67d0dec386df
izeye <izeye@naver.com>
2015-04-05 09:29:50 +0900

    Change `Object.clone()` to `Arrays.copyOf()` based on `System.arraycopy()` for performance.

--- src/main/java/samples/springboot/util/ArrayUtils.java
+++ src/main/java/samples/springboot/util/ArrayUtils.java
@@ -1,5 +1,7 @@
 package samples.springboot.util;
 
+import java.util.Arrays;
+
 /**
  * Created by izeye on 15. 4. 5..
  */
@@ -8,7 +10,7 @@ public class ArrayUtils {
 	public static int[][] copy(int[][] src) {
 		int[][] dst = new int[src.length][];
 		for (int i = 0; i < src.length; i++) {
-			dst[i] = src[i].clone();
+			dst[i] = Arrays.copyOf(src[i], src[i].length);
 		}
 		return dst;
 	}
