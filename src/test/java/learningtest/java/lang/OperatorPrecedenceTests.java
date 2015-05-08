790f202c274ce5838aac84da0d48faf9f0c02125
izeye <izeye@naver.com>
2015-05-08 20:33:27 +0900

    Add an operator precedence test.

---
+++ src/test/java/learningtest/java/lang/OperatorPrecedenceTests.java
@@ -0,0 +1,19 @@
+package learningtest.java.lang;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by izeye on 15. 5. 8..
+ */
+public class OperatorPrecedenceTests {
+	
+	@Test
+	public void testCastAndMultiply() {
+		assertThat(((int) 10.1 * 100), is(1000));
+		assertThat(((int) (10.1 * 100)), is(1010));
+	}
+	
+}
