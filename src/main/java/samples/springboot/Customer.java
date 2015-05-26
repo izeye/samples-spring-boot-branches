64d22806cec0bb7a42243d9f702fd62445a45833
izeye <izeye@naver.com>
2015-05-27 00:13:30 +0900

    Remove unused imports.

--- src/main/java/samples/springboot/Customer.java
+++ src/main/java/samples/springboot/Customer.java
@@ -1,8 +1,5 @@
 package samples.springboot;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
