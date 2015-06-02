fee849211983a93ef9942e2524455cc1d7cf85d2
izeye <izeye@naver.com>
2015-06-02 18:38:24 +0900

    Add an MDC sample.

--- src/test/java/learningtest/ch/qos/logback/LogbackTests.java
+++ src/test/java/learningtest/ch/qos/logback/LogbackTests.java
@@ -2,6 +2,7 @@ package learningtest.ch.qos.logback;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
+import org.apache.log4j.MDC;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,14 @@ public class LogbackTests {
 	public void testStatusPrinter() {
 		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 		StatusPrinter.print(lc);
+	}
+	
+	@Test
+	public void testMdc() {
+		logger.debug("Test MDC.");
+		
+		MDC.put("sessionId", 1234);
+		logger.debug("Test MDC.");
 	}
 
 }
