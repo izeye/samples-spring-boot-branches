1fb0f7516bf4a7c4bd7a9afad05ce1297163f63a
izeye <izeye@naver.com>
2015-06-15 17:53:18 +0900

    Configure Spring Boot Actuator.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -3,23 +3,19 @@ package samples.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Created by izeye on 15. 1. 31..
  */
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
 		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
-
-		try {
-			Thread.sleep(Integer.MAX_VALUE);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
