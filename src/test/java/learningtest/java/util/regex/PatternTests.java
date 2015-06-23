ad3be1f64d9fc6b2b8bc8d964d6746d2257b334b
izeye <izeye@naver.com>
2015-06-23 16:27:01 +0900

    Add a `Pattern` sample.

---
+++ src/test/java/learningtest/java/util/regex/PatternTests.java
@@ -0,0 +1,29 @@
+package learningtest.java.util.regex;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by izeye on 15. 6. 23..
+ */
+public class PatternTests {
+	
+	@Test
+	public void test() {
+		String expected = "http://localhost/persons/search/name=John&age=35";
+		
+		String url = "http://localhost/persons/search/name=Johnny&age=35";
+		Pattern pattern = Pattern.compile("name=(.+)&");
+		Matcher matcher = pattern.matcher(url);
+		matcher.find();
+		String name = matcher.group(1);
+		String newName = "John";
+		assertThat(url.replace(name, newName), is(expected));
+	}
+	
+}
