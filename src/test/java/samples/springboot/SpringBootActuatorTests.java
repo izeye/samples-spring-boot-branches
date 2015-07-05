1d75351f87aa521084a30139e761639302205c6e
izeye <izeye@naver.com>
2015-07-05 18:45:59 +0900

    Test `DropwizardMetricServices`.

--- src/test/java/samples/springboot/SpringBootActuatorTests.java
+++ src/test/java/samples/springboot/SpringBootActuatorTests.java
@@ -2,11 +2,18 @@ package samples.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 /**
  * Created by izeye on 15. 6. 29..
@@ -20,12 +27,20 @@ public class SpringBootActuatorTests {
 	int port;
 
 	RestTemplate restTemplate = new RestTemplate();
+	
+	@Autowired
+	CounterService counterService;
 
 	@Test
 	public void testMetrics() {
 		String metrics = restTemplate.getForObject(
 				"http://localhost:{port}/management/metrics", String.class, port);
 		System.out.println(metrics);
+	}
+	
+	@Test
+	public void testCounterService() {
+		assertThat(counterService, is(instanceOf(DropwizardMetricServices.class)));
 	}
 	
 }
