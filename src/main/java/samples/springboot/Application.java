6458b9e15a16b9d1f49269b2986d2aa8a8e1ad0b
izeye <izeye@naver.com>
2015-07-06 20:38:20 +0900

    Reproduce a YAML problem with `spring-boot-devtools`.

--- src/main/java/samples/springboot/Application.java
+++ src/main/java/samples/springboot/Application.java
@@ -1,14 +1,34 @@
 package samples.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * Created by izeye on 15. 1. 31..
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	private final Yaml yaml = new Yaml(new Constructor(Person.class));
+
+	@Override
+	public void run(String... args) throws Exception {
+		File file = ResourceUtils.getFile("classpath:data/person.yml");
+		Object object = yaml.load(new FileInputStream(file));
+		System.out.println(Person.class.getClassLoader());
+		System.out.println(object.getClass().getClassLoader());
+		
+		Person person = (Person) object;
+		System.out.println(person);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
@@ -21,5 +41,5 @@ public class Application {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
---
+++ src/main/java/samples/springboot/Person.java
@@ -0,0 +1,18 @@
+package samples.springboot;
+
+/**
+ * Created by izeye on 15. 7. 6..
+ */
+public class Person {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
