5bf226f0b84fe2d9422e8c25b9b1d5c1c74d034b
izeye <izeye@naver.com>
2015-08-24 09:50:58 +0900

    Add a Gmail SMTP sample.

--- src/test/java/samples/springboot/ApplicationTests.java
+++ src/test/java/samples/springboot/ApplicationTests.java
@@ -1,9 +1,15 @@
 package samples.springboot;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by izeye on 15. 3. 1..
@@ -11,9 +17,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
+	
+	@Autowired
+	JavaMailSender javaMailSender;
 
 	@Test
-	public void test() {
+	@Ignore
+	public void testSend() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("izeye@naver.com");
+		message.setSubject("Test");
+		message.setText("This is a test.");
+		javaMailSender.send(message);
 	}
 
 }
