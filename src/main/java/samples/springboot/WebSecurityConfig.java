804aa349ce84282c984a65d126928927c14d0e1c
izeye <izeye@naver.com>
2015-09-15 14:16:49 +0900

    Upgrade to Spring Boot 1.3.0.M5.

--- src/main/java/samples/springboot/WebSecurityConfig.java
+++ src/main/java/samples/springboot/WebSecurityConfig.java
@@ -12,7 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		super.configure(http);
+		super.configure(http);
 
 		http.formLogin();
 	}
